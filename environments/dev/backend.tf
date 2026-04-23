9d258b4c6a240cb57f3346998aea3f4140712763
MikeGyver <mikejgarcia@gmail.com>
2026-04-22 19:39:12 -0600

    Update main branch to be landing page for various setups in branches.

--- environments/dev/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "s3" {
-    # Leave this completely empty. 
-    # Your -backend-config flag will fill in the details.
-  }
-}
--- modules/apigw/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "rest_api_id" {
-  value = aws_api_gateway_rest_api.this.id
-}
-
-output "stage_name" {
-  value = aws_api_gateway_stage.this.stage_name
-}
